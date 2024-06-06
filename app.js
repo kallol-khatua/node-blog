b28df858ed3c2bce5f7b73efcd2f112185df6e19
Kallol Khatua <kallolkhatua2005@gmail.com>
2024-06-06 23:09:20 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,38 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const path = require("path");
+const ejsMate = require("ejs-mate");
+
+// routers
+const blogRouter = require("./routes/blogRouter.js");
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "/public")));
+app.engine("ejs", ejsMate);
+
+// connection to mongodb
+mongoose.connect(process.env.DBURI)
+    .then(() => {
+        console.log("Connected to DB");
+        const port = process.env.PORT || 8080;
+        app.listen(port, () => {
+            console.log(`Listening to port ${port}`);
+        })
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+
+app.get("/", (req, res) => {
+    res.redirect("/blogs")
+})
+
+app.use("/blogs", blogRouter);
+
+app.all("*", (req, res) => {
+    res.render("404.ejs", {title: "Page not found"});
+})
